d99149c764d2cfe8f5a1e7668ece8e842ef7ceae
Harichandana8786 <chandanachandana8786@gmail.com>
2026-05-20 14:52:30 +0530

    Added fetch news module

---
+++ modules/fetch_news.py
@@ -0,0 +1,14 @@
+import requests
+from config import URL
+
+def fetch_news():
+
+    response = requests.get(URL)
+
+    if response.status_code != 200:
+        print("Failed to fetch news")
+        print(response.text)
+        return None
+
+    return response.json()
+
